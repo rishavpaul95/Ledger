8562b5bccfa5c140866ecfa979d7a8a9793f6391
Rishav Paul <rishavpaul1995@gmail.com>
2022-01-29 09:47:15 +0530

    v7.0

--- Experiment/app/controllers/application_controller.rb
+++ Experiment/app/controllers/application_controller.rb
@@ -3,12 +3,19 @@ class ApplicationController < ActionController::Base
 
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+     around_action :user_time_zone, if: :current_user
+   
+
     protected
 
+          def user_time_zone(&block)
+               Time.use_zone(current_user.time_zone, &block)
+          end
+
          def configure_permitted_parameters
-              devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+              devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :time_zone)}
 
-              devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
+              devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password , :time_zone) }
          end
-
+                        
 end
--- Experiment/db/schema.rb
+++ Experiment/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_19_121753) do
+ActiveRecord::Schema.define(version: 2022_01_28_000547) do
 
   create_table "calculations", force: :cascade do |t|
     t.string "value"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2022_01_19_121753) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.string "time_zone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
